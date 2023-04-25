b3e0570c10e0b3f3c639290d6cbc1bc8856ec7ba
ChetanG_111 <84891627+ChetanG111@users.noreply.github.com>
2023-04-25 18:40:15 +0530

    P4
    Severe Changes
    Layout Completed
    All Displays Now Update
    Working on Code Box

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const consoleInput = document.getElementById('console-input')
+const consoleInput = document.getElementById('codeBox')
 const goalDis = document.getElementById('goal')
 const titleDisplay = document.getElementById('titleDis')
 const levelDisplay = document.getElementById('lDis')
+const topicDis = document.getElementById('A')
 
 
 let lIndex = 1;
 
-let goals = {
-    '1' : ['Select the Cookie'],
-    '2' : ['Select the Cookies']
+let topic = {
+    '1' : 'document.getElementByID("elementID")',
+    '2' : 'document.getElementsByClassName("classNames")'
+}
+
+let goal = {
+    '1' : 'Select the Cookie',
+    '2' : 'Select the Cookies'
 }
 
 let sols = {
@@ -22,7 +28,7 @@ let title = {
 }
 
 consoleInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+  if (event.key === '`') {
     event.preventDefault();
 
     const code = consoleInput.value;
@@ -39,27 +45,25 @@ consoleInput.addEventListener('keydown', (event) => {
 
     if(!flag)
     {
-        console.log(lIndex)
+        consoleInput.value = 'Wrong👎❌😡'
     }
-    consoleInput.value = '';
   }
 });
 
 
 function LevelPassed()
 {
-    consoleOutput.value += ">> You Passed the level\n";
-    if(lIndex < Object.keys(goals).length)
+    if(lIndex < Object.keys(goal).length)
     {
         lIndex += 1;
     }
-    console.log(lIndex)
     UpdateDisplays()
 }
 
 function UpdateDisplays()
 {
-    goalDis.innerHTML = goals[lIndex]
+    goalDis.innerHTML = goal[lIndex]
     titleDisplay.innerHTML = title[lIndex]
     levelDisplay.innerHTML = 'Level ' + lIndex.toString() + ' of 32'
+    topicDis.innerHTML = topic[lIndex]
 }
